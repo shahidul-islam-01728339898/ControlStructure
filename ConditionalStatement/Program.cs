365fa983e596d4102dae90a1381ab2acdefdbb27
Md. Shahidul Islam <shahidulislam339898@gmail.com>
2025-06-12 11:57:30 +0600

    01. Check if a number is positive, negative, or zero

--- ConditionalStatement/Program.cs
+++ ConditionalStatement/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Basic Numeric Comparison Example
+// 01. Check if a number is positive, negative, or zero
+
+Console.Write("Enter A Number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > 0)
+{
+    Console.WriteLine($"The {num1} is a positive number.");
+}
+else if (num1<0)
+{
+    Console.WriteLine($" The {num1} is a negative number.");
+}
+else
+{
+    Console.WriteLine($"This input number is {num1}");
+}
