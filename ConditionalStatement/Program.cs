38946c5b12b699ff432869bad950ecf63b8b7886
Md. Shahidul Islam <shahidulislam339898@gmail.com>
2025-06-12 12:28:46 +0600

    //02.Check the two integers are equal/greater or not

--- ConditionalStatement/Program.cs
+++ ConditionalStatement/Program.cs
@@ -2,6 +2,8 @@
 // Basic Numeric Comparison Example
 // 01. Check if a number is positive, negative, or zero
 
+using System.Transactions;
+
 Console.Write("Enter A Number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -9,11 +11,38 @@ if (num1 > 0)
 {
     Console.WriteLine($"The {num1} is a positive number.");
 }
-else if (num1<0)
+else if (num1 < 0)
 {
     Console.WriteLine($" The {num1} is a negative number.");
 }
 else
 {
-    Console.WriteLine($"This input number is {num1}");
+    Console.WriteLine($"This input number is {num1}/ Zeor");
+}
+
+
+//02.Check the two integers are equal/greater or not
+
+Console.Write("Enter Your First Number: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Second Number: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine($"The First number: {number1} and the Second number :{number2} are equal");
+}
+else if(number1> number2)
+{
+    Console.WriteLine($"The First number: {number1} is greater than the Second number: {number2}");
+
+}
+else if(number2>number1)
+{
+    Console.WriteLine($"The Second number: {number2} is greater than the First number: {number1}");
+}
+else
+{
+    Console.WriteLine($"The First number: {number1} and the Second number: {number2} is a general number");
 }
